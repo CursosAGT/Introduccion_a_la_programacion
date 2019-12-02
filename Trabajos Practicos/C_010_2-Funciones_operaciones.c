3c3345a08067c1225999b1e77637f3609f79e527
AGT1973 <55587133+CursosAGT@users.noreply.github.com>
2019-12-02 08:57:54 -0300

    Add files via upload

---
+++ Trabajos Practicos/C_010_2-Funciones_operaciones.c
@@ -0,0 +1,40 @@
+// Copyright 2019 Ariel H Garcia Traba <cursos.agt@gmail.com>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//Ej 006_2
+
+int main()
+{
+
+	printf("\n+------------------------+");
+	printf("\n|     Examen Final       |");
+	printf("\n|                        |");
+	printf("\n+------------------------+");
+
+int array_final[20]={15,8,51,74,9,12,35,17,15,23,99,33,62,24,83,21,7,99,15,46};
+printf("\n\nEjercicio"
+"\n\t\tNivel 0"			//-------nivel 0
+"\n\tDado el array de 20 elementos, averigua si el valor ubicado en [0] se repite y cuantas veces"
+"\n\tDado el array de 20 elementos, averigua la media y la varianza de los elementos del vector."
+"\n\tDado el array de 20 elementos, genera uno nuevo ordenando los elementos de un vector de mayor a menor valor."
+"\n\tDado el array de 20 elementos, genera uno nuevo invirtiendo los elementos de un vector el ultimo al primero y viceversa."
+"\n\tIngresa in numero enteros e imprime averigua si esta. "
+"\n\tDado el array de 20 elementos, Cual es su Minimo Comun Divisor."
+"\n\tNota  Ver archivo MCD.c"
+"\n\t\tPara hallar el Máximo Común Divisor de los números por divisiones sucesivas, haremos lo siguiente:"
+"\n\t\t1º Dividimos el mayor por el menor, si el resto es cero, el divisor (el menor) es el M.C.D de los números."
+"\n\t\t2º Si el resto no es cero, se divide nuevamente el divisor entre el resto.");
+"\n\t\tNivel 1"			//-------nivel 1
+"\n\tUsando funciones"
+"\n\tIngresa in numero enteros e imprime cual es el mas cercano menor y mayor."
+"\n\timprime si es la mitad o el doble de algun valor de los 20 elementos de array."
+}
+
